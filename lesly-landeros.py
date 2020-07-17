c8f946ceda463bef8aaf65653af073dfd5581858
Lesly Abigail Landeros Sanchez <leslylanderossanchez@gmail.com>
2020-07-17 10:29:32 -0500

    Definicion De Clase

---
+++ lesly-landeros.py
@@ -0,0 +1,12 @@
+class Mouse: 
+    def _int_(self, marca, color, tipo):
+        self._marca= marca
+        self._color= color
+        self._tipo= tipo
+
+@property
+def marca(self):
+    return self._marca
+    
+
+    
