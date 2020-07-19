be1107bd4e74ecc60b1d425add5baf50482c6aa6
Lesly Abigail Landeros Sanchez <leslylanderossanchez@gmail.com>
2020-07-19 01:35:14 -0500

    TERMINADO

--- lesly-landeros.py
+++ lesly-landeros.py
@@ -4,13 +4,42 @@ class Mouse:
         self._color= color
         self._tipo= tipo
 
-     @property
+    @property
     def marca(self):
-            return self._marca
+        return self._marca
 
     @marca.setter
     def marca: (self, valor):
         self._marca= valor
+
+    @property
+    def color(self):
+        return self._color 
+    
+    @color.setter
+    def color: (self, valor):
+        self._color= valor 
+    
+    @property
+    def tipo(self):
+        return self._tipo 
+
+    @tipo.setter
+    def tipo: (self, valor):
+        self._tipo= valor 
+    
+    def imprimir (self):
+        print (f'Mouse: {self.marca}, tipo: {self.tipo}, color: {sel.color}')
+        
+
+
+
+
+
+    
+
+
+
     
 
 
